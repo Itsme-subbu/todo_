6be5208689b6ab28bdeb4ad9e2b0d835f5b227de
Venkat Subbarao Bandaru <subbaraobandaru30@gmail.com>
2025-12-09 13:18:06 +0530

    update

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -19,7 +19,8 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
 ]
--- todo_project/views.py
+++ todo_project/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from todo_app.models import Task
+# Create your views here.
 def home(request):
-    return render(request,'home.html')
+    tasks = Task.objects.filter(is_completed=False)
+    context = {
+        'tasks': tasks,
+    }
+
+    return render(request, 'home.html', context)
