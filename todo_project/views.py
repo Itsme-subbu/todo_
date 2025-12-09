01bb9eb940736b657e8f020ae05c420f11acbe3b
Venkat Subbarao Bandaru <subbaraobandaru30@gmail.com>
2025-12-09 20:43:28 +0530

    update

--- todo_project/views.py
+++ todo_project/views.py
@@ -3,8 +3,10 @@ from todo_app.models import Task
 # Create your views here.
 def home(request):
     tasks = Task.objects.filter(is_completed=False)
+    incompleteTasks = Task.objects.filter(is_completed = True)
     context = {
         'tasks': tasks,
+        'incompleteTask' : incompleteTasks
     }
 
     return render(request, 'home.html', context)
